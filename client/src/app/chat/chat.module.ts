94afe1effdcea4e17d89e8c224e1224745c90f07
Narcwis <narcis.cristea@gmail.com>
2018-06-25 00:44:00 +0100

    added emoji picker

--- client/src/app/chat/chat.module.ts
+++ client/src/app/chat/chat.module.ts
@@ -8,16 +8,18 @@ import { ChatComponent } from './chat.component';
 import { SocketService } from './shared/services/socket.service';
 import { DialogUserComponent } from './dialog-user/dialog-user.component';
 import { DialogInfoComponent } from './dialog-info/dialog-info.component';
+import { PickerModule } from '@ctrl/ngx-emoji-mart'
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    MaterialModule
+    MaterialModule,
+    PickerModule
   ],
-  declarations: [ChatComponent, DialogUserComponent, DialogInfoComponent],
-  providers: [SocketService],
-  entryComponents: [DialogUserComponent, DialogInfoComponent]
+  declarations: [ ChatComponent, DialogUserComponent, DialogInfoComponent ],
+  providers: [ SocketService ],
+  entryComponents: [ DialogUserComponent, DialogInfoComponent ]
 })
 export class ChatModule { }
